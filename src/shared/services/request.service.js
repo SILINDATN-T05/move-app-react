3433b0c52a3567835e0670437858ee707874a099
Thulani Silinda <silindatn@gmail.com>
2018-07-30 18:05:00 +0200

    all

---
+++ src/shared/services/request.service.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import { environment } from './service.config';
+
+axios.interceptors.request.use(function (config) {
+
+    var token = sessionStorage.getItem('token');
+  
+    if (token) {
+      config.headers['x-access-token'] = token;
+    }
+  
+    // config.headers['X-Requested-With'] = 'XMLHttpRequest';
+    // config.headers['Expires'] = '-1';
+    // config.headers['Cache-Control'] = "no-cache,no-store,must-revalidate,max-age=-1,private";
+  
+    return config;
+  
+  }, function (err) {
+  
+    return Promise.reject(err);
+  });
+
+export class RequestService {
+    postRequest(uri, body, callback){
+
+        body['channel'] = environment.portal.channel;
+        body['application'] = environment.portal.application;
+
+        axios.post(environment.portal.serverUrl + uri, body)
+        .then((response) => {
+            callback(response.data);
+        })
+        .catch((error) => {
+            if(error.response) {
+                callback({code: error.response.status, message: 'service.request.failed'});
+            } else {
+                callback({code: '404', message: 'service.request.failed'});
+            }
+        })
+    }
+    getRequest(uri, callback) {
+        axios.get(environment.portal.serverUrl + uri)
+        .then((response) => {
+            callback(response);
+        })
+        .catch((error) => {
+            if(error.response) {
+                callback({code: error.response.status, message: 'service.request.failed'});
+            } else {
+                callback({code: '404', message: 'service.request.failed'});
+            }
+        })
+    }
+}
---
+++ src/shared/services/service.config.js
@@ -0,0 +1,18 @@
+export const environment = {
+    production: false,
+    version: '(dev)',
+    portal: {
+        serverUrl1: 'http://10.0.0.102:3090/api',
+        serverUrl: 'http://localhost:3090/api',
+        defaultLanguage: 'en-US',
+        channel: 'PORTAL',
+        application: 'MOVE-PORTAL',
+        supportedLanguages: [
+            'en-US',
+            'fr-FR',
+        ],
+    },
+    systems: [
+      'SILINDATN',
+    ],
+  };
